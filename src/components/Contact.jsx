48a5cf5794026931970147e6dc894744bbdcf6cd
king-Abby <iyiolaabby@gmail.com>
2024-11-08 11:56:32 +0100

    Fifth commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { toast, ToastContainer,   } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Contact = () => {
